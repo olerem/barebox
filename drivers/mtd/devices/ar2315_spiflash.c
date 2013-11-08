0d406248f81b8ad7e6332c1e7fc7cf5d803106e0
Oleksij Rempel <linux@rempel-privat.de>
2013-11-08 09:23:45 +0100

    ar2315_spiflash.c: some more be_le conversatio
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- drivers/mtd/devices/ar2315_spiflash.c
+++ drivers/mtd/devices/ar2315_spiflash.c
@@ -153,7 +153,7 @@ static int ar2315sf_write_then_read(struct ar2315sf_priv *priv,
 	/* TODO, recheck endians or do endian conversation */
 	rx_tmp[0] = le32_to_cpu(reg_readl(priv, SPI_FLASH_DATA));
 	/* opcode regiser can be used for data on read */
-	rx_tmp[1] = reg_readl(priv, SPI_FLASH_OPCODE);
+	rx_tmp[1] = le32_to_cpu(reg_readl(priv, SPI_FLASH_OPCODE));
 
 	memcpy(rx_buf, &rx_tmp[0], rx_size);
 	return 0;
