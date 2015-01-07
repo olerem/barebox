a6819ccc733ed85bda91263ffdba4216146c7506
Oleksij Rempel <linux@rempel-privat.de>
2015-01-07 21:23:51 +0100

    arch/arm/mach-alphascale/lowlevel-asm9260.c
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- arch/arm/mach-alphascale/lowlevel-asm9260.c
+++ arch/arm/mach-alphascale/lowlevel-asm9260.c
@@ -1,12 +1,27 @@
+/*
+ * (C) Copyright 2014 Oleksij Rempel <linux@rempel-privat.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
 
 #include <common.h>
 #include <sizes.h>
 #include <asm/barebox-arm-head.h>
 #include <asm/barebox-arm.h>
 #include <asm/system_info.h>
+#include <mach/asm9260-regs.h>
 
-void __naked barebox_arm_reset_vector(void)
+void __naked __noreturn barebox_arm_reset_vector(void)
 {
 	arm_cpu_lowlevel_init();
-	barebox_arm_entry(0x20680000, SZ_32M, NULL);
+
+	barebox_arm_entry(ASM9260_MEMORY_BASE, SZ_32M, NULL);
 }
