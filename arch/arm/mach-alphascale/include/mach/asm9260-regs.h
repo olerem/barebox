85772cda793a8d2b4763fe0a78544458e3b7c4f1
Oleksij Rempel <linux@rempel-privat.de>
2015-01-07 20:52:46 +0100

    add include/mach/asm9260-regs.h
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

---
+++ arch/arm/mach-alphascale/include/mach/asm9260-regs.h
@@ -0,0 +1,20 @@
+/*
+ * (C) Copyright 2014 Oleksij Rempel <linux@rempel-privat.de>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __ASM_SOC_ASM9260_REGS_H
+#define __ASM_SOC_ASM9260_REGS_H
+
+#define ASM9260_MEMORY_BASE		0x20000000
+
+#endif /* __ASM_SOC_ASM9260_REGS_H */
