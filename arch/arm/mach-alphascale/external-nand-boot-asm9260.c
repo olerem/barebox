fae712786964e318be89c5b3dc95340c626f327e
Oleksij Rempel <linux@rempel-privat.de>
2015-01-09 16:47:36 +0100

    arch/arm/mach-alphascale/external-nand-boot-asm9260.c
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- arch/arm/mach-alphascale/external-nand-boot-asm9260.c
+++ arch/arm/mach-alphascale/external-nand-boot-asm9260.c
@@ -11,9 +11,6 @@ u32 nand_spare_size = 36;
 
 #define MemClr(p,n) memset(p,0,n)
 
-/*
-* 等待NAND控制器ready
-*/
 static int NandWaitForControllerReady(void)
 {
 	int ret = 0;
@@ -31,9 +28,6 @@ static int NandWaitForControllerReady(void)
 	return ret;
 }
 
-/*
-* 等待NAND设备ready
-*/
 static int NandWaitForDeviceReady(u8 nChip)
 {
 	int ret = 0;
@@ -56,9 +50,6 @@ static __inline nand_info *GetNandInfo(void)
 	return (nand_info *)(&alp_nandinfo);
 }
 
-/*
-* 读nLen个数据到pBuffer地址中
-*/
 static void NandReadBuffer(unsigned char *pBuffer, u32 nLen)
 {
 	u32 i;
@@ -75,32 +66,6 @@ static void NandReadBuffer(unsigned char *pBuffer, u32 nLen)
 	}
 }
 
-#if 0
-/*
-* 设置NAND的PIN MUX
-*/
-static void NandSetPinMux(void)
-{
-    	SetPinMux(11,0,5);
-	SetPinMux(11,1,5);    
-	SetPinMux(11,2,5);    
-	SetPinMux(11,3,5);    
-	SetPinMux(11,4,5);    
-	SetPinMux(11,5,5);    
-	SetPinMux(11,6,5);    
-	SetPinMux(12,0,5);    
-	SetPinMux(12,1,5);    
-	SetPinMux(12,2,5);    
-	SetPinMux(12,3,5);    
-	SetPinMux(12,4,5);    
-	SetPinMux(12,5,5);    
-	SetPinMux(12,6,5);    
-	SetPinMux(12,7,5);
-}
-#endif
-/*
-* NAND芯片复位
-*/
 int NandFlashReset(u8 nChip)
 {
     outl((0xff00 | nChip), NAND_MEM_CTRL);
@@ -112,9 +77,6 @@ int NandFlashReset(u8 nChip)
 	return NandWaitForDeviceReady(nChip);
 }
 
-/*
-* 设置NAND控制器时序
-*/
 static int NandTimingConf(void)
 {
 	int ret = 0;
@@ -167,9 +129,6 @@ static int NandTimingConf(void)
 	return ret;
 }
 
-/*
-* 为选定的NAND芯片设置片选的PIN MUX
-*/
 static void NandSelectChip(unsigned char nChip)
 {
     outl((0xff00 |  nChip), NAND_MEM_CTRL);
@@ -262,15 +221,6 @@ void select_ecc(int page_size,int oob_size)
 	}
 }
 
-/*******************************************************************************
-*函数名:	NandInit
-*功能:		初始化NAND控制器，包括GPIO，选择NAND芯片，设置时序。并且复位NAND FLASH
-*输入参数:	nChip--选择的Chip
-*输出参数:	无
-*返回值:	0--表示成功
-*			其他值对应各种错误
-*NOTE:		
-*******************************************************************************/
 int NandInit(unsigned char nChip)
 {
 	int ret = 0;
@@ -298,34 +248,20 @@ int NandInit(unsigned char nChip)
 	return ret;
 }
 
-/*******************************************************************************
-*函数名:	NandLargePageMakeAddr
-*功能:		设置将要发送到控制器的地址，控制器将地址处理后发送给NAND。
-*输入参数:	nPage--NAND页数
-*			oob_flag--设置的如果是OOB的地址，该参数需为1，否则为0
-*输出参数:	pAddr--设置后地址的保存处
-*返回值:	无
-*NOTE:		1、调用方需保证存放地址的内存，
-*			该驱动申请了全局数组NandAddr[32]专用于设置地址的存放
-*			2、擦除时，控制器会自动取行地址发给NAND，因此不需要进行专门的处理
-*******************************************************************************/
 static void NandLargePageMakeAddr(u32 nPage, u32 nColumn, unsigned char *pAddr)
 {
 	int i = 0;
 	u32 row_addr = nPage;
 	nand_info *pNandInfo = GetNandInfo();
 
-	//清空
 	memset(pAddr, 0, pNandInfo->addr_cycles);
 
-	//设置列地址
 	for (i=0; i<pNandInfo->col_cycles; i++)
 	{
 		pAddr[i] = (unsigned char)(nColumn & 0xFF);
 		nColumn = nColumn >> 8;
 	}
-	
-	//设置行地址,其实就是nPage页号
+
 	for (i = pNandInfo->col_cycles; i < pNandInfo->addr_cycles; i++)
 	{
 		pAddr[i] = (unsigned char)(row_addr & 0xFF);		//字节掩码
@@ -333,21 +269,10 @@ static void NandLargePageMakeAddr(u32 nPage, u32 nColumn, unsigned char *pAddr)
 	}
 }
 
-/*******************************************************************************
-*函数名:	NandReadIDPio
-*功能:		读NAND FLASH的ID
-*输入参数:	nChip--选择的Chip
-*			pDevId--读出的ID存放的地址
-*			nByte--读出ID的字节数
-*输出参数:	无
-*返回值:	0--表示成功
-*			其他值对应各种错误
-*NOTE:		
-*******************************************************************************/
 int NandReadIDPio(unsigned char nChip, unsigned int *pDevId, unsigned int nByte)
 {
 	int ret = 0;
-	
+
 	outl(1, NAND_FIFO_INIT);		/*复位FIFO*/
 	outl(nByte, NAND_DATA_SIZE);
 	outl((0xff00 | nChip), NAND_MEM_CTRL);
@@ -357,19 +282,16 @@ int NandReadIDPio(unsigned char nChip, unsigned int *pDevId, unsigned int nByte)
 	   | (INPUT_SEL_BIU << 6)
 	   | (SEQ1), NAND_COMMAND);
 	ret = NandWaitForDeviceReady(nChip);
-	
+
 	pDevId[0] = inl(NAND_FIFO_DATA);
 
 	return ret;
 }
 
-/*
-* 配置NAND控制器
-*/
 static void NandControllerConfig (void)
 {
 	nand_info *pNandInfo = GetNandInfo();
-	
+
 	outl((EN_STATUS << 23)				| (NO_RNB_SEL << 22)
 	   | (BIG_BLOCK_EN << 21)	 		| (pNandInfo->addr_cycles << 18)
 	   | (ADDR1_AUTO_INCR_DIS << 17) 	| (ADDR0_AUTO_INCR_DIS << 16)
@@ -382,9 +304,6 @@ static void NandControllerConfig (void)
 
 }
 
-/*
-* NAND的硬件ECC校验函数
-*/
 static int NandEccCheck(void)
 {
 	int ret = 0;
@@ -410,23 +329,6 @@ static int NandEccCheck(void)
 	return ret;
 }
 
-
-/*******************************************************************************
-*函数名:	NandReadLargePagePio
-*功能:		大页NAND读函数
-*输入参数:	nPage--NAND的页数
-*			nColumn--该页的列
-*			pBuffer--读出的内存首地址
-*			nLen--读出的字节数
-*输出参数:	无
-*返回值:	0--表示成功
-*			其他值对应各种错误
-*详细说明:	1、支持写oob(无ECC校验)
-*			2、支持整页读(有ECC校验)
-*			3、支持自定义长度读
-*			   可随意读，但列地址加上读取长度必须小于页大小加可利用的oob
-*			4、只支持2K及以上页大小NAND的读
-*******************************************************************************/
 static int NandReadLargePagePio(u32 nPage, u32 nColumn, unsigned char *pBuffer, u32 nLen)
 {
 	int ret = 0;
@@ -434,7 +336,6 @@ static int NandReadLargePagePio(u32 nPage, u32 nColumn, unsigned char *pBuffer, 
 	u32 *addr = (u32 *)NandAddr;
 	nand_info *pNandInfo = GetNandInfo();
 
-	/*1、复位FIFO，配置NAND控制器*/
 	outl(0x1, NAND_FIFO_INIT);
 	NandControllerConfig( );
 
@@ -451,33 +352,27 @@ static int NandReadLargePagePio(u32 nPage, u32 nColumn, unsigned char *pBuffer, 
 		outl(nLen, NAND_DATA_SIZE);
 	}
 
-	/*2、选择chip，配置NAND地址*/
 	outl((0xff00 | pNandInfo->chip_select), NAND_MEM_CTRL);
 	NandLargePageMakeAddr(nPage, nColumn, NandAddr);
 	outl(addr[0], NAND_ADDR0_L);
 	outl(addr[1], NAND_ADDR0_H);
 
-	/*3、发起命令*/
 	outl((READ_PAGE_2<<16)
 	   | (READ_PAGE_1<<8)
 	   | (ADDR_SEL_0<<7)
 	   | (INPUT_SEL_BIU<<6)
 	   | (SEQ10), NAND_COMMAND);
 
-	/*4、等待控制器ready*/
 	ret = NandWaitForControllerReady();
 	if (ret != 0)
 	{
 		return ret;
 	}
 
-	/*5、读数据*/
 	NandReadBuffer(tmpbuf, nLen);
 
-	/*6、如果ECC使能，判断ECC是否正确*/
 	if ((inl(NAND_CONTROL) & (ECC_EN<<5)) != 0)
 	{
-		
 		ret = NandEccCheck( );
 	}
 	return ret;
@@ -491,7 +386,7 @@ static int nand_block_is_bad(int page)
 
 	bad_byte[0] = bad_byte[1] = bad_byte[2] = bad_byte[3] = 0xff;
 	NandReadLargePagePio(page, pNandInfo->page_size, bad_byte, 4);
-	
+
 	if ((bad_byte[0] != 0xff) || (bad_byte[1] != 0xff))
 		return -1;
 
