81172d8d5e0814df0811f6617387bb1e43a12fe5
Oleksij Rempel <linux@rempel-privat.de>
2015-02-18 09:38:19 +0100

    add reset/alphascale,asm9260-rst.h
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

---
+++ include/dt-bindings/reset/alphascale,asm9260-rst.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2015, Oleksij Rempel <linux@rempel-privat.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#ifndef _DT_BINDINGS_RESET_ALPHASCALE_ASM9260_RST_H
+#define _DT_BINDINGS_RESET_ALPHASCALE_ASM9260_RST_H
+
+#define GPIO_RESET	4
+#define MAC_RESET	5
+#define EMI_RESET	6
+#define USB0_RESET	7
+#define USB1_RESET	8
+#define DMA0_RESET	9
+#define DMA1_RESET	10
+#define UART0_RESET	11
+#define UART1_RESET	12
+#define UART2_RESET	13
+#define UART3_RESET	14
+#define UART4_RESET	15
+#define UART5_RESET	16
+#define UART6_RESET	17
+#define UART7_RESET	18
+#define UART8_RESET	19
+#define UART9_RESET	20
+#define I2S0_RESET	21
+#define I2C0_RESET	22
+#define I2C1_RESET	23
+#define SSP0_RESET	24
+#define IOCON_RESET	25
+#define WDT_RESET	26
+#define CAN0_RESET	27
+#define CAN1_RESET	28
+#define MPWM_RESET	29
+#define SPI0_RESET	30
+#define SPI1_RESET	31
+#define QEI_RESET	32
+#define QUADSPI0_RESET	33
+#define CAMIF_RESET	34
+#define LCDIF_RESET	35
+#define CT32B0_RESET	36
+#define CT32B1_RESET	37
+#define CT32B2_RESET	38
+#define CT32B3_RESET	39
+#define IRQ_RESET	40
+#define RTC_RESET	41
+#define NAND_RESET	42
+#define ADC0_RESET	42
+#define LED_RESET	43
+#define DAC0_RESET	44
+#define LCD_RESET	45
+#define I2S1_RESET	46
+#define MAC1_RESET	47
+
+#endif
