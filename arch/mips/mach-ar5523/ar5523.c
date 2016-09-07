d4dd7f1b903a28346e3c61b45e7a916383f2ff25
Oleksij Rempel <fixed-term.Oleksij.Rempel@de.bosch.com>
2016-09-07 14:09:07 +0200

    arch/mips/mach-ar5523/ar5523.c
    Signed-off-by: Oleksij Rempel <fixed-term.Oleksij.Rempel@de.bosch.com>

--- arch/mips/mach-ar5523/ar5523.c
+++ arch/mips/mach-ar5523/ar5523.c
@@ -12,75 +12,15 @@
 #include <io.h>
 #include <platform_data/serial-ns16550.h>
 #include <mach/ar5523_regs.h>
-#if 0
-
-/*
- * This table is indexed by bits 5..4 of the CLOCKCTL1 register
- * to determine the predevisor value.
- */
-static int CLOCKCTL1_PREDIVIDE_TABLE[4] = { 1, 2, 4, 5 };
-
-static unsigned int
-ar5523_cpu_frequency(void)
-{
-	unsigned int predivide_mask, predivide_shift;
-	unsigned int multiplier_mask, multiplier_shift;
-	unsigned int clock_ctl1, pre_divide_select, pre_divisor, multiplier;
-	unsigned int doubler_mask;
-	u32 devid;
-
-	devid = __raw_readl((char *)KSEG1ADDR(AR5523_REV));
-	devid &= AR5523_REV_MAJ;
-	devid >>= AR5523_REV_MAJ_S;
-	if (devid == AR5523_REV_MAJ_AR2313) {
-		predivide_mask = AR2313_CLOCKCTL1_PREDIVIDE_MASK;
-		predivide_shift = AR2313_CLOCKCTL1_PREDIVIDE_SHIFT;
-		multiplier_mask = AR2313_CLOCKCTL1_MULTIPLIER_MASK;
-		multiplier_shift = AR2313_CLOCKCTL1_MULTIPLIER_SHIFT;
-		doubler_mask = AR2313_CLOCKCTL1_DOUBLER_MASK;
-	} else { /* AR5312 and AR5523 */
-		predivide_mask = AR5523_CLOCKCTL1_PREDIVIDE_MASK;
-		predivide_shift = AR5523_CLOCKCTL1_PREDIVIDE_SHIFT;
-		multiplier_mask = AR5523_CLOCKCTL1_MULTIPLIER_MASK;
-		multiplier_shift = AR5523_CLOCKCTL1_MULTIPLIER_SHIFT;
-		doubler_mask = AR5523_CLOCKCTL1_DOUBLER_MASK;
-	}
-
-	/*
-	 * Clocking is derived from a fixed 40MHz input clock.
-	 *
-	 *  cpuFreq = InputClock * MULT (where MULT is PLL multiplier)
-	 *  sysFreq = cpuFreq / 4	   (used for APB clock, serial,
-	 *				    flash, Timer, Watchdog Timer)
-	 *
-	 *  cntFreq = cpuFreq / 2	   (use for CPU count/compare)
-	 *
-	 * So, for example, with a PLL multiplier of 5, we have
-	 *
-	 *  cpuFreq = 200MHz
-	 *  sysFreq = 50MHz
-	 *  cntFreq = 100MHz
-	 *
-	 * We compute the CPU frequency, based on PLL settings.
-	 */
-
-	clock_ctl1 = __raw_readl((char *)KSEG1ADDR(AR5523_CLOCKCTL1));
-	pre_divide_select = (clock_ctl1 & predivide_mask) >> predivide_shift;
-	pre_divisor = CLOCKCTL1_PREDIVIDE_TABLE[pre_divide_select];
-	multiplier = (clock_ctl1 & multiplier_mask) >> multiplier_shift;
-
-	if (clock_ctl1 & doubler_mask)
-		multiplier = multiplier << 1;
-
-	return (40000000 / pre_divisor) * multiplier;
-}
 
 static unsigned int
 ar5523_sys_frequency(void)
 {
-	return ar5523_cpu_frequency() / 4;
+	/* for now just use statick clock */
+	return AR5523_WIFI_CLK;
 }
 
+#if 0
 /*
  * shutdown watchdog
  */
@@ -101,8 +41,8 @@ static int platform_init(void)
 	return 0;
 }
 late_initcall(platform_init);
-
 #endif
+
 static struct NS16550_plat serial_plat = {
 	.shift = AR5523_UART_SHIFT,
 };
@@ -120,10 +60,9 @@ static int ar5523_console_init(void)
 	reset &= ~AR5523_RESET_UART0;
 	__raw_writel(reset, (char *)KSEG1ADDR(AR5523_RESET));
 
+#endif
 	/* Register the serial port */
 	serial_plat.clock = ar5523_sys_frequency();
-#endif
-	serial_plat.clock = 40000000;
 	add_ns16550_device(DEVICE_ID_DYNAMIC, KSEG1ADDR(AR5523_UART0),
 			   8 << AR5523_UART_SHIFT,
 			   IORESOURCE_MEM | IORESOURCE_MEM_8BIT,
