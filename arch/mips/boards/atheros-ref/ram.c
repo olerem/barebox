75a557ea8fb94a2a63416c1dbf87977ee80f49eb
Oleksij Rempel <fixed-term.Oleksij.Rempel@de.bosch.com>
2016-09-07 12:29:20 +0200

    add board make file
    Signed-off-by: Oleksij Rempel <fixed-term.Oleksij.Rempel@de.bosch.com>

---
+++ arch/mips/boards/atheros-ref/ram.c
@@ -0,0 +1,14 @@
+#include <common.h>
+#include <init.h>
+#include <linux/sizes.h>
+#include <asm/memory.h>
+
+static int mem_init(void)
+{
+	barebox_set_model("Netgear wg102");
+	barebox_set_hostname("wg102");
+
+//	mips_add_ram0(SZ_16M);
+	return 0;
+}
+mem_initcall(mem_init);
