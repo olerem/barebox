5a0433319d0eeae3b30f2bf9fa7c298ef211b6fb
Oleksij Rempel <linux@rempel-privat.de>
2017-07-16 07:30:59 +0200

    add 9344 debug_ll
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

---
+++ arch/mips/mach-ath79/include/mach/debug_ll.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (C) 2013 Antony Pavlov <antonynpavlov@gmail.com>
+ *
+ * This file is part of barebox.
+ * See file CREDITS for list of people who contributed to this project.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __MACH_AR9344_DEBUG_LL__
+#define __MACH_AR9344_DEBUG_LL__
+
+#include <mach/debug_ll_ar9344.h>
+
+#endif /* __MACH_AR9344_DEBUG_LL_H__ */
