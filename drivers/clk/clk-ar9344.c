a7c56715094b85bf01acdf590c13526249de7009
Oleksij Rempel <linux@rempel-privat.de>
2017-09-21 19:49:24 +0200

    m drivers/clk/clk-ar9344.c
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- drivers/clk/clk-ar9344.c
+++ drivers/clk/clk-ar9344.c
@@ -26,10 +26,28 @@
 #include <mach/ath79.h>
 #include <dt-bindings/clock/ath79-clk.h>
 
+#define AR9344_CPU_PLL_CONFIG			0x00
+#define AR9344_DDR_PLL_CONFIG			0x04
+#define AR9344_OUTDIV_M				0x3
+#define AR9344_OUTDIV_S				19
+#define AR9344_REFDIV_M				0x1f
+#define AR9344_REFDIV_S				12
+#define AR9344_NINT_M				0x3f
+#define AR9344_NINT_S				6
+#define AR9344_NFRAC_M				0x3f
+#define AR9344_NFRAC_S				0
+
+
+#define AR9344_CPU_DDR_CLOCK_CONTROL		0x08
+#define AR9344_CPU_FROM_CPUPLL			BIT(20)
+#define AR9344_CPU_PLL_BYPASS			BIT(2)
+#define AR9344_CPU_POST_DIV_M			0x1f
+#define AR9344_CPU_POST_DIV_S			5
+
 static struct clk *clks[ATH79_CLK_END];
 static struct clk_onecell_data clk_data;
 
-struct clk_ar933x {
+struct clk_ar9344 {
 	struct clk clk;
 	void __iomem *base;
 	u32 div_shift;
@@ -37,60 +55,49 @@ struct clk_ar933x {
 	const char *parent;
 };
 
-static unsigned long clk_ar933x_recalc_rate(struct clk *clk,
+static unsigned long clk_ar9344_recalc_rate(struct clk *clk,
 	unsigned long parent_rate)
 {
-	struct clk_ar933x *f = container_of(clk, struct clk_ar933x, clk);
-	unsigned long rate;
-	unsigned long freq;
+	struct clk_ar9344 *f = container_of(clk, struct clk_ar9344, clk);
+	int outdiv, refdiv, nint, nfrac;
+	int cpu_post_div;
 	u32 clock_ctrl;
-	u32 cpu_config;
-	u32 t;
+	u32 val;
 
-	clock_ctrl = __raw_readl(f->base + AR933X_PLL_CLOCK_CTRL_REG);
-
-	if (clock_ctrl & AR933X_PLL_CLOCK_CTRL_BYPASS) {
-		rate = parent_rate;
+	clock_ctrl = __raw_readl(f->base + AR9344_CPU_DDR_CLOCK_CONTROL);
+	cpu_post_div = ((clock_ctrl >> AR9344_CPU_POST_DIV_S)
+			& AR9344_CPU_POST_DIV_M) + 1;
+	if (clock_ctrl & AR9344_CPU_PLL_BYPASS) {
+		return parent_rate;
+	} else if (clock_ctrl & AR9344_CPU_FROM_CPUPLL) {
+		val = __raw_readl(f->base + AR9344_CPU_PLL_CONFIG);
 	} else {
-		cpu_config = __raw_readl(f->base + AR933X_PLL_CPU_CONFIG_REG);
-
-		t = (cpu_config >> AR933X_PLL_CPU_CONFIG_REFDIV_SHIFT) &
-		    AR933X_PLL_CPU_CONFIG_REFDIV_MASK;
-		freq = parent_rate / t;
-
-		t = (cpu_config >> AR933X_PLL_CPU_CONFIG_NINT_SHIFT) &
-		    AR933X_PLL_CPU_CONFIG_NINT_MASK;
-		freq *= t;
-
-		t = (cpu_config >> AR933X_PLL_CPU_CONFIG_OUTDIV_SHIFT) &
-		    AR933X_PLL_CPU_CONFIG_OUTDIV_MASK;
-		if (t == 0)
-			t = 1;
-
-		freq >>= t;
-
-		t = ((clock_ctrl >> f->div_shift) & f->div_mask) + 1;
-		rate = freq / t;
+		val = __raw_readl(f->base + AR9344_DDR_PLL_CONFIG);
 	}
 
-	return rate;
+	outdiv = (val >> AR9344_OUTDIV_S) & AR9344_OUTDIV_M;
+	refdiv = (val >> AR9344_REFDIV_S) & AR9344_REFDIV_M;
+	nint = (val >> AR9344_NINT_S) & AR9344_NINT_M;
+	nfrac = (val >> AR9344_NFRAC_S) & AR9344_NFRAC_M;
+
+	return (parent_rate * (nint + (nfrac >> 9))) / (refdiv * (1 << outdiv));
 }
 
-struct clk_ops clk_ar933x_ops = {
-	.recalc_rate = clk_ar933x_recalc_rate,
+struct clk_ops clk_ar9344_ops = {
+	.recalc_rate = clk_ar9344_recalc_rate,
 };
 
-static struct clk *clk_ar933x(const char *name, const char *parent,
-	void __iomem *base, u32 div_shift, u32 div_mask)
+static struct clk *clk_ar9344(const char *name, const char *parent,
+	void __iomem *base)
 {
-	struct clk_ar933x *f = xzalloc(sizeof(*f));
+	struct clk_ar9344 *f = xzalloc(sizeof(*f));
 
 	f->parent = parent;
 	f->base = base;
-	f->div_shift = div_shift;
-	f->div_mask = div_mask;
+	f->div_shift = 0;
+	f->div_mask = 0;
 
-	f->clk.ops = &clk_ar933x_ops;
+	f->clk.ops = &clk_ar9344_ops;
 	f->clk.name = name;
 	f->clk.parent_names = &f->parent;
 	f->clk.num_parents = 1;
@@ -100,22 +107,12 @@ static struct clk *clk_ar933x(const char *name, const char *parent,
 	return &f->clk;
 }
 
-static void ar933x_pll_init(void __iomem *base)
+static void ar9344_pll_init(void __iomem *base)
 {
-	clks[ATH79_CLK_CPU] = clk_ar933x("cpu", "ref", base,
-		AR933X_PLL_CLOCK_CTRL_CPU_DIV_SHIFT,
-		AR933X_PLL_CLOCK_CTRL_CPU_DIV_MASK);
-
-	clks[ATH79_CLK_DDR] = clk_ar933x("ddr", "ref", base,
-		AR933X_PLL_CLOCK_CTRL_DDR_DIV_SHIFT,
-		AR933X_PLL_CLOCK_CTRL_DDR_DIV_MASK);
-
-	clks[ATH79_CLK_AHB] = clk_ar933x("ahb", "ref", base,
-		AR933X_PLL_CLOCK_CTRL_AHB_DIV_SHIFT,
-		AR933X_PLL_CLOCK_CTRL_AHB_DIV_MASK);
+	clks[ATH79_CLK_CPU] = clk_ar9344("cpu", "ref", base);
 }
 
-static int ar933x_clk_probe(struct device_d *dev)
+static int ar9344_clk_probe(struct device_d *dev)
 {
 	struct resource *iores;
 	void __iomem *base;
@@ -125,7 +122,7 @@ static int ar933x_clk_probe(struct device_d *dev)
 		return PTR_ERR(iores);
 	base = IOMEM(iores->start);
 
-	ar933x_pll_init(base);
+	ar9344_pll_init(base);
 
 	clk_data.clks = clks;
 	clk_data.clk_num = ARRAY_SIZE(clks);
@@ -135,22 +132,22 @@ static int ar933x_clk_probe(struct device_d *dev)
 	return 0;
 }
 
-static __maybe_unused struct of_device_id ar933x_clk_dt_ids[] = {
+static __maybe_unused struct of_device_id ar9344_clk_dt_ids[] = {
 	{
-		.compatible = "qca,ar9330-pll",
+		.compatible = "qca,ar9344-pll",
 	}, {
 		/* sentinel */
 	}
 };
 
-static struct driver_d ar933x_clk_driver = {
-	.probe	= ar933x_clk_probe,
-	.name	= "ar933x_clk",
-	.of_compatible = DRV_OF_COMPAT(ar933x_clk_dt_ids),
+static struct driver_d ar9344_clk_driver = {
+	.probe	= ar9344_clk_probe,
+	.name	= "ar9344_clk",
+	.of_compatible = DRV_OF_COMPAT(ar9344_clk_dt_ids),
 };
 
-static int ar933x_clk_init(void)
+static int ar9344_clk_init(void)
 {
-	return platform_driver_register(&ar933x_clk_driver);
+	return platform_driver_register(&ar9344_clk_driver);
 }
-postcore_initcall(ar933x_clk_init);
+postcore_initcall(ar9344_clk_init);
