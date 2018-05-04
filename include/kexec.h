6a7800d5c77323e132f1ab91ab539444a10a6dfe
Oleksij Rempel <linux@rempel-privat.de>
2018-05-04 08:45:25 +0200

    m include/kexec.h
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- include/kexec.h
+++ include/kexec.h
@@ -1,14 +1,6 @@
 #ifndef _LINUX_REBOOT_H
 #define _LINUX_REBOOT_H
 
-/*
- * Commands accepted by the _reboot() system call.
- *
- * KEXEC       Restart system using a previously loaded Linux kernel
- */
-
-#define	LINUX_REBOOT_CMD_KEXEC		0x45584543
-
-extern int kexec_arch(int cmd, void *opaque);
+extern int kexec_arch(void *opaque);
 
 #endif /* _LINUX_REBOOT_H */
