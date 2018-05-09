5c3f8677ca7e59a66fc62807aa006d2189fa6e64
Oleksij Rempel <linux@rempel-privat.de>
2018-05-09 11:47:20 +0200

    m include/kexec.h
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- include/kexec.h
+++ include/kexec.h
@@ -104,9 +104,8 @@ struct mem_ehdr {
 };
 
 void free_elf_info(struct mem_ehdr *ehdr);
-int build_elf_info(const char *buf, size_t len, struct mem_ehdr *ehdr, u32 flags);
-int build_elf_exec_info(const char *buf, off_t len,
-				struct mem_ehdr *ehdr, u32 flags);
+int build_elf_info(const char *buf, size_t len, struct mem_ehdr *ehdr);
+int build_elf_exec_info(const char *buf, off_t len, struct mem_ehdr *ehdr);
 
 int elf_exec_load(struct mem_ehdr *ehdr, struct kexec_info *info);
 
