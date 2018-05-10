f34ecd03c9e85439ab7f274685aafba8fc4009cf
Oleksij Rempel <linux@rempel-privat.de>
2018-05-10 08:02:07 +0200

    m arch/mips/mach-malta/kexec.c
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- arch/mips/mach-malta/kexec.c
+++ arch/mips/mach-malta/kexec.c
@@ -1,27 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0
 /*
  * Copyright (C) 2014, 2016 Antony Pavlov <antonynpavlov@gmail.com>
- *
- * This file is part of barebox.
- * See file CREDITS for list of people who contributed to this project.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
  */
 
+#include <asm/io.h>
+#include <boot.h>
 #include <common.h>
 #include <init.h>
-#include <memory.h>
-#include <boot.h>
 #include <kexec.h>
-#include <bootm.h>
-#include <asm/io.h>
+#include <memory.h>
 
 #define ENVP_ADDR	0x80002000l
 #define ENVP_NB_ENTRIES	16
