111f9310d83c4f684d4c9e5510c4c9cefd84b253
Oleksij Rempel <o.rempel@pengutronix.de>
2018-08-06 13:47:01 +0200

    fix the clock
    Signed-off-by: Oleksij Rempel <o.rempel@pengutronix.de>

--- arch/mips/mach-mediatek/include/mach/debug_ll.h
+++ arch/mips/mach-mediatek/include/mach/debug_ll.h
@@ -7,5 +7,16 @@
 #define __MACH_MEDIATEK_DEBUG_LL__
 
 
+#define DEBUG_LL_UART_ADDR	KSEG1ADDR(0x10000e00)
+#define DEBUG_LL_UART_SHIFT	2
+/*
+ * system clk = 40Mhz
+ * HIGH SPEED UART div = 16 (UARTn+0024h HIGH SPEED UART register == 0)
+ */
+#define DEBUG_LL_UART_CLK       (40000000 / 16)
+#define DEBUG_LL_UART_BPS       CONFIG_BAUDRATE
+#define DEBUG_LL_UART_DIVISOR   (DEBUG_LL_UART_CLK / DEBUG_LL_UART_BPS)
+
+#include <asm/debug_ll_ns16550.h>
 
 #endif /* __MACH_MEDIATEK_DEBUG_LL__ */
