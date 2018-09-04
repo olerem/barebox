154d9371e193241f9591735906259f6c0941cd73
Oleksij Rempel <o.rempel@pengutronix.de>
2018-09-04 11:02:59 +0200

    m arch/mips/boards/seeed_studio-linkit_smart_7688/include/board/board_pbl_start.h

--- arch/mips/boards/seeed_studio-linkit_smart_7688/include/board/board_pbl_start.h
+++ arch/mips/boards/seeed_studio-linkit_smart_7688/include/board/board_pbl_start.h
@@ -18,6 +18,17 @@
 
 	debug_ll_outc '1'
 
+	/* test if we are in the flash */
+	pbl_blt 0xbf000000 skip_pll_ram_config t8
+	debug_ll_outc '4'
+
+	/* Initialize caches... */
+	mips_cache_reset
+
+	/* ... and enable them */
+	dcache_enable
+skip_pll_ram_config:
+	debug_ll_outc '6'
 	debug_ll_ns16550_outnl
 
 	mips_nmon
