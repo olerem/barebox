43bb4233c0da97bb0b9de53ef89c441bbf1ef4f4
Oleksij Rempel <o.rempel@pengutronix.de>
2018-10-10 16:44:43 +0200

    MIPS: multiimage: add ENTRY_FUNCTION macros
    Signed-off-by: Oleksij Rempel <o.rempel@pengutronix.de>

--- arch/mips/include/asm/asm.h
+++ arch/mips/include/asm/asm.h
@@ -80,6 +80,35 @@ symbol:		.frame	sp, framesize, rpc
 symbol:
 
 /*
+ * ENTRY_FUNCTION - mark start of entry function
+ */
+#define ENTRY_FUNCTION(symbol)				\
+	.set noreorder;					\
+	.section .text_head_entry.BOARD_PBL_START;	\
+	.align 4;					\
+							\
+EXPORT(symbol)
+
+/*
+ * ENTRY_FUNCTION_END - mark end of entry function
+ */
+#define ENTRY_FUNCTION_END(symbol,dtb_start,dtb_end)	\
+	mips_nmon;					\
+	copy_to_link_location	symbol;			\
+	stack_setup;					\
+							\
+	la	a0, dtb_start;				\
+	la	a1, dtb_end;				\
+	la	v0, pbl_main_entry;			\
+	jal	v0;					\
+	 nop;						\
+							\
+	/* No return */					\
+__error:						\
+	b	__error;				\
+	 nop;
+
+/*
  * FEXPORT - export definition of a function symbol
  */
 #define FEXPORT(symbol)					\
