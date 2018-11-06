f4e41ddc33ccc5ee9758f2f97d6ab9b9fa8b2e6f
Oleksij Rempel <linux@rempel-privat.de>
2018-11-06 10:23:59 +0100

    m arch/mips/mach-xburst/include/mach/debug_ll.h
    Signed-off-by: Oleksij Rempel <linux@rempel-privat.de>

--- arch/mips/mach-xburst/include/mach/debug_ll.h
+++ arch/mips/mach-xburst/include/mach/debug_ll.h
@@ -21,7 +21,15 @@
 /** @file
  *  This File contains declaration for early output support
  */
-//#include <board/debug_ll.h>
+
+#ifdef CONFIG_DEBUG_JZ4750D_UART
+#include <mach/debug_ll_jz4750d.h>
+#elif defined CONFIG_DEBUG_JZ4780_UART
+#include <mach/debug_ll_jz4780.h>
+#else
+#error "unknown xburst debug uart soc type"
+#endif
+
 #include <asm/debug_ll_ns16550.h>
 
 #endif  /* __MACH_XBURST_DEBUG_LL__ */
