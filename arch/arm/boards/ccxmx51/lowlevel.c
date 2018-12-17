76f6795cbd8e8bd8259249eccbcec9cc85fd22ff
Alexander Shiyan <shc_work@mail.ru>
2018-12-17 08:51:49 +0300

    ARM: CCMX51: Switch to multiimage support
    This is a cumulative patch for the Digi ConnectCore CCMX51 SOM.
    It includes:
    - Switch board to devicetree probe.
    - Add MMC update handler.
    - Switch to multiimage support.
    - Cleanup and optimize board code.
    
    Signed-off-by: Alexander Shiyan <shc_work@mail.ru>
    Signed-off-by: Sascha Hauer <s.hauer@pengutronix.de>

--- arch/arm/boards/ccxmx51/lowlevel.c
+++ arch/arm/boards/ccxmx51/lowlevel.c
@@ -5,8 +5,16 @@
 #include <asm/barebox-arm-head.h>
 #include <mach/imx51-regs.h>
 
-void __naked barebox_arm_reset_vector(void)
+ENTRY_FUNCTION(start_ccxmx51, r0, r1, r2)
 {
+	extern char __dtb_imx51_ccxmx51_start[];
+	void *fdt;
+
 	imx5_cpu_lowlevel_init();
-	barebox_arm_entry(MX51_CSD0_BASE_ADDR, SZ_128M, NULL);
+
+	arm_setup_stack(0x20000000 - 16);
+
+	fdt = __dtb_imx51_ccxmx51_start + get_runtime_offset();
+
+	barebox_arm_entry(MX51_CSD0_BASE_ADDR, SZ_128M, fdt);
 }
