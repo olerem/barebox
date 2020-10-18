690437b00d4b184f8cb03b5f6d00cd50952d0738
Oleksij Rempel <o.rempel@pengutronix.de>
2020-10-18 19:37:20 +0200

    m arch/mips/mach-ar231x/include/mach/debug_ll.h
    Signed-off-by: Oleksij Rempel <o.rempel@pengutronix.de>

--- arch/mips/mach-ar231x/include/mach/debug_ll.h
+++ arch/mips/mach-ar231x/include/mach/debug_ll.h
@@ -9,8 +9,8 @@
 #include <mach/ar2312_regs.h>
 
 #define DEBUG_LL_UART_ADDR		KSEG1ADDR(AR2312_UART0)
-#define DEBUG_LL_UART_SHIFT AR2312_UART_SHIFT
-#define DEBUG_LL_UART_DIVISOR 2
+#define DEBUG_LL_UART_SHIFT		AR2312_UART_SHIFT
+#define DEBUG_LL_UART_DIVISOR		(45000000 / (16 * CONFIG_BAUDRATE))
 
 /** @file
  *  This File contains declaration for early output support
