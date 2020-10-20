55e84802e75313b46181f0a98be3e80db07efa9d
Oleksij Rempel <o.rempel@pengutronix.de>
2020-10-20 19:50:10 +0200

    m dts/include/dt-bindings/clock/ath25-clk.h
    Signed-off-by: Oleksij Rempel <o.rempel@pengutronix.de>

--- dts/include/dt-bindings/clock/ath25-clk.h
+++ dts/include/dt-bindings/clock/ath25-clk.h
@@ -4,11 +4,8 @@
 #define __DT_BINDINGS_ATH25_CLK_H
 
 #define ATH25_CLK_CPU		0
-#define ATH25_CLK_DDR		1
-#define ATH25_CLK_AHB		2
-#define ATH25_CLK_REF		3
-#define ATH25_CLK_MDIO		4
+#define ATH25_CLK_APB		1
 
-#define ATH25_CLK_END		5
+#define ATH25_CLK_END		2
 
 #endif /* __DT_BINDINGS_ATH25_CLK_H */
